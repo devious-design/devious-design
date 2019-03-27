b391f7d024cf8618617402d081875f752f5c5d4d
Stephen Esser <Stephen.Esser@Cerner.com>
2019-03-27 18:54:00 -0500

    Generate an embedded client directory using React

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	// Comment out this line:
+  //res.send('respond with a resource');
+
+  // And insert something like this instead:
+  res.json([{
+  	id: 1,
+  	username: "samsepi0l"
+  }, {
+  	id: 2,
+  	username: "D0loresH4ze"
+  }]);
 });
 
 module.exports = router;

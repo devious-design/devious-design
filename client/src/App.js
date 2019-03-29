a80e624a732927d7045f52b64a0b2ec19e1b3811
Stephen Esser <Stephen.Esser@Cerner.com>
2019-03-29 15:37:23 -0500

    Update the base app display

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  state = {users: []}
-
-  componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
-        )}
-      </div>
-    );
-  }
-}
+const App = () => (
+  <h1 className="heading">
+    Welcome to Devious Design
+  </h1>
+)
 
 export default App;
